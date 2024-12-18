92429ff1317067940cc74c9e0b197cf8443ce8da
Abdullah Elseginy <abdullah.ahmed.elseginy@gmail.com>
2024-12-18 16:43:37 +0200

    changes

--- src/screens/Projects/index.jsx
+++ src/screens/Projects/index.jsx
@@ -50,6 +50,7 @@ const Projects = () => {
                 <h3 className="text-xl font-semibold text-white mb-2">
                   {item.title}
                 </h3>
+
                 <p className="text-lg text-gray-200">{item.description}</p>
               </div>
 
